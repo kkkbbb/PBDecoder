44a15c85a33a57cdb719921ba4a4a4b60776432e
Miles Routson <1687907308@qq.com>
2024-07-25 22:39:33 +0800

    修复
    添加group类型自动识别为一个具体的message类

--- src/com/wwb/proto/jebscript.py
+++ src/com/wwb/proto/jebscript.py
@@ -21,7 +21,7 @@ class test(IScript):
     
     className = ctx.displayForm("proto class","the class of protobuf",
     FormEntry.Text('className', '', FormEntry.INLINE, None, 0, 0))[0]
-    protoresult = self.parseCls(dexUnit.getClass("L"+className+";"));
+    protoresult = self.parseCls(dexUnit.getClass("L"+className+";"))
     ctx.displayText("proto", protoresult,True)
     
   def parseCls(self,cls):
@@ -38,21 +38,20 @@ class test(IScript):
         continue
       field = fields.split("=")[0].split(" ")
       mfieldType =field[1] if not "oneof" in field[1] else field[0]
-      if mfieldType == "message":
+      if mfieldType == "message" or mfieldType == "group":
         for clsField in cls.getFields():
           if clsField.getName() == field[2]:
             mtype = clsField.getFieldType()
-            cresultstr += "\t" + fields.replace("message",mtype.getName()) + "\n"
+            cresultstr += "\t" + fields.replace(mfieldType,mtype.getName()) + "\n"
             if mtype.getName() in parsedClass: continue
             subresult += self.parseCls(mtype.getImplementingClass())
         continue
-
+          
       if mfieldType == "enum":
         fields = fields.replace("enum","int32") + " //unknow enum"
       cresultstr +="\t"+fields+"\n"
       
       if not self.isBaseType(mfieldType):
-        #print mfieldType
         mfieldType = mfieldType.strip()
         if mfieldType in parsedClass: continue
         subresult += self.parseCls(dexUnit.getClass("L"+mfieldType+";"))
@@ -60,7 +59,7 @@ class test(IScript):
     return cresultstr+"}\n\n"+subresult
   
   def isBaseType(self,mtype):
-    for basetype in ["enum","string","int","float","bool","fixed","bytes","oneof","map"]:
+    for basetype in ["enum","string","int","float","bool","fixed","bytes","oneof","map","group"]:
       if basetype in mtype:
         return True
     return False
@@ -68,42 +67,45 @@ class test(IScript):
 
   def parseProto(self,cls):
     for method in cls.getMethods():
-      if method.getName() != "<init>" and method.getName() != "<clinit>":
-        codeItem = method.getData().getCodeItem()
-        
-        objs = ""
-        if isinstance(codeItem, IDexCodeItem):
-          instructions = codeItem.getInstructions()
-          for firststr,ins in enumerate(instructions):
-            if ins.getMnemonic() == "const-string":
-              break
-            
-          if firststr == len(instructions)-1: continue  #inccorect method!
-          while True:
-            ins = instructions[firststr]
-            firststr+=1
-            #print ins
-            if ins.getMnemonic() == "const-string":
-              conststr = dexUnit.getString(ins.getOperands()[1].getValue()).getValue()
-              if len(conststr) > 2 or "\x01" in conststr or "\x02" in conststr:
-                messageinfo = conststr
-              else:
-                objs+=conststr+","
-              continue
-            if ins.getMnemonic() == "const-class":
-              objs+= dexUnit.getType(ins.getParameters()[1].getValue()).getName()+","
-              continue
-            if ins.getMnemonic() == "sget-object":
-              objs+="enum.type,"
-              continue
-            if "filled-new-array" in ins.getMnemonic() or "invoke-static" == ins.getMnemonic():
-              break
-            else:
-              raise Exception("Unknow Instruction!")
-          break
+      if method.getName() == "<init>" or method.getName() == "<clinit>": continue
 
+      codeItem = method.getData().getCodeItem()
+      
+      objs = ""
+      messageinfo = ""
+      if isinstance(codeItem, IDexCodeItem):
+        instructions = codeItem.getInstructions()
+        for firststr,ins in enumerate(instructions):
+          if ins.getMnemonic() == "const-string":
+            break
+        
+        if firststr == len(instructions)-1: continue  #inccorect method!
+        while True:
+          ins = instructions[firststr]
+          firststr+=1
+          # print ins
+          if ins.getMnemonic() == "const-string":
+            conststr = dexUnit.getString(ins.getOperands()[1].getValue()).getValue()
+            if len(conststr) > 2 or "\x01" in conststr or "\x02" in conststr:
+              messageinfo = conststr
+            else:
+              objs+=conststr+","
+            continue
+          if ins.getMnemonic() == "const-class":
+            objs+= dexUnit.getType(ins.getParameters()[1].getValue()).getName()+","
+            continue
+          if ins.getMnemonic() == "sget-object":
+            objs+="enum.type,"
+            continue
+          if "invoke-static" == ins.getMnemonic() or firststr >= len(instructions)-1:
+            break
+        
+        if len(messageinfo) < 2: continue
+        else: break
+            
+    #print cls,method
+    if len(messageinfo) < 2: raise Exception("Unexcept messageinfo!")
     if len(objs) < 1: return ""
-    #print messageinfo,objs
     return PBMain.forJeb(self.to_unicode_escape(messageinfo),objs)
       
   def to_unicode_escape(self,s):
