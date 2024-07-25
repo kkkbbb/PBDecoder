5010f8e870bdabdcd24a8703d3def2e627631e34
Miles Routson <1687907308@qq.com>
2024-07-25 11:40:09 +0800

    jeb脚本bug
    修复部分protobuf类解析出错，原因：除了包含messgeinfo的方法，还有多个方法

--- src/com/wwb/proto/jebscript.py
+++ src/com/wwb/proto/jebscript.py
@@ -11,99 +11,98 @@ from com.wwb.proto import PBMain
 
 dexUnit = None
 parsedClass = []
-
+    
 class test(IScript):
-    def run(self, ctx):
-        global dexUnit,parsedClass
-        prj = ctx.getMainProject()
-        dexUnit = prj.findUnit(IDexUnit)
-        parsedClass = []
+  def run(self, ctx):
+    global dexUnit,parsedClass
+    prj = ctx.getMainProject()
+    dexUnit = prj.findUnit(IDexUnit)
+    parsedClass = []
+    
+    className = ctx.displayForm("proto class","the class of protobuf",
+    FormEntry.Text('className', '', FormEntry.INLINE, None, 0, 0))[0]
+    protoresult = self.parseCls(dexUnit.getClass("L"+className+";"));
+    ctx.displayText("proto", protoresult,True)
+    
+  def parseCls(self,cls):
+    parsedClass.append(cls.getName())
+    currentproto = self.parseProto(cls)
+    #print currentproto
+    
+    cresultstr = "message " + cls.getName() + " {\n"
+    subresult = ""
+    for fields in currentproto.split("\n"):
+      if not len(fields) > 0: continue
+      if "{" in fields or "}" in fields:
+        cresultstr +="\t"+fields+"\n"
+        continue
+      field = fields.split("=")[0].split(" ")
+      mfieldType =field[1] if not "oneof" in field[1] else field[0]
+      if mfieldType == "message":
+        for clsField in cls.getFields():
+          if clsField.getName() == field[2]:
+            mtype = clsField.getFieldType()
+            cresultstr += "\t" + fields.replace("message",mtype.getName()) + "\n"
+            if mtype.getName() in parsedClass: continue
+            subresult += self.parseCls(mtype.getImplementingClass())
+        continue
+            
+      cresultstr +="\t"+fields+"\n"
+      
+      if not self.isBaseType(mfieldType):
+        #print mfieldType
+        mfieldType = mfieldType.strip()
+        if mfieldType in parsedClass: continue
+        subresult += self.parseCls(dexUnit.getClass("L"+mfieldType+";"))
+        
+    return cresultstr+"}\n\n"+subresult
+  
+  def isBaseType(self,mtype):
+    for basetype in ["enum","string","int","int","bool","fixed","bytes","oneof"]:
+      if basetype in mtype:
+        return True
+    return False
+    
 
-        className = ctx.displayForm("proto class","the class of protobuf",
-                                    FormEntry.Text('className', '', FormEntry.INLINE, None, 0, 0))[0]
-        protoresult = self.parseCls(dexUnit.getClass("L"+className+";"));
-        ctx.displayText("proto", protoresult,True)
-
-    def parseCls(self,cls):
-        parsedClass.append(cls.getName())
-        currentproto = self.parseProto(cls)
-        #print currentproto
-
-        cresultstr = "message " + cls.getName() + " {\n"
-        subresult = ""
-        for fields in currentproto.split("\n"):
-            if not len(fields) > 0: continue
-            if "{" in fields or "}" in fields:
-                cresultstr +="\t"+fields+"\n"
-                continue
-            field = fields.split("=")[0].split(" ")
-            mfieldType =field[1] if not "oneof" in field[1] else field[0]
-            if mfieldType == "message":
-                for clsField in cls.getFields():
-                    if clsField.getName() == field[2]:
-                        mtype = clsField.getFieldType()
-                        cresultstr += "\t" + fields.replace("message",mtype.getName()) + "\n"
-                        if mtype.getName() in parsedClass: continue
-                        subresult += self.parseCls(mtype.getImplementingClass())
-                continue
-
-            cresultstr +="\t"+fields+"\n"
-
-            if not self.isBaseType(mfieldType):
-                #print mfieldType
-                mfieldType = mfieldType.strip()
-                if mfieldType in parsedClass: continue
-                subresult += self.parseCls(dexUnit.getClass("L"+mfieldType+";"))
-
-        return cresultstr+"}\n\n"+subresult
-
-    def isBaseType(self,mtype):
-        for basetype in ["enum","string","int","int","bool","fixed","bytes","oneof"]:
-            if basetype in mtype:
-                return True
-        return False
-
-
-    def parseProto(self,cls):
-        for method in cls.getMethods():
-            if method.getName() != "<init>" and method.getName() != "<clinit>":
-                break
+  def parseProto(self,cls):
+    for method in cls.getMethods():
+      if method.getName() != "<init>" and method.getName() != "<clinit>":
         codeItem = method.getData().getCodeItem()
-
+        
         objs = ""
         if isinstance(codeItem, IDexCodeItem):
-            instructions = codeItem.getInstructions()
-            for firststr,ins in enumerate(instructions):
-                if ins.getMnemonic() == "const-string":
-                    break
+          instructions = codeItem.getInstructions()
+          for firststr,ins in enumerate(instructions):
+            if ins.getMnemonic() == "const-string":
+              break
+            
+          if firststr == len(instructions)-1: continue  #inccorect method!
+          while True:
+            ins = instructions[firststr]
+            firststr+=1
+            #print ins
+            if ins.getMnemonic() == "const-string":
+              conststr = dexUnit.getString(ins.getOperands()[1].getValue()).getValue()
+              if len(conststr) > 2 or "\x01" in conststr or "\x02" in conststr:
+                messageinfo = conststr
+              else:
+                objs+=conststr+","
+              continue
+            if ins.getMnemonic() == "const-class":
+              objs+= dexUnit.getType(ins.getParameters()[1].getValue()).getName()+","
+              continue
+            if ins.getMnemonic() == "sget-object":
+              objs+="enum.type,"
+              continue
+            if "filled-new-array" in ins.getMnemonic() or "invoke-static" == ins.getMnemonic():
+              break
+            else:
+              raise Exception("Unknow Instruction!")
+          break
 
-            while True:
-                ins = instructions[firststr]
-                firststr+=1
-                if ins.getMnemonic() == "const-string":
-                    conststr = dexUnit.getString(ins.getOperands()[1].getValue()).getValue()
-                    if len(conststr) > 2 or "\x01" in conststr or "\x02" in conststr:
-                        messageinfo = conststr
-                    else:
-                        objs+=conststr+","
-                    continue
-                if ins.getMnemonic() == "const-class":
-                    objs+= dexUnit.getType(ins.getParameters()[1].getValue()).getName()+","
-                    continue
-                if ins.getMnemonic() == "sget-object":
-                    objs+="enum.type,"
-                    continue
-                if "filled-new-array" in ins.getMnemonic() or "invoke-static" == ins.getMnemonic():
-                    break
-                else:
-                    raise Exception("Unknow Instruction!")
-
-            if len(objs) < 1: return ""
-            #print messageinfo,objs
-            return PBMain.forJeb(self.to_unicode_escape(messageinfo),objs)
-
-    def to_unicode_escape(self,s):
-        return ''.join('\\u%04X' % ord(c) if ord(c) <= 0xFFFF else '\\u%08X' % ord(c) for c in s)
-
-
-
+    if len(objs) < 1: return ""
+    #print messageinfo,objs
+    return PBMain.forJeb(self.to_unicode_escape(messageinfo),objs)
+      
+  def to_unicode_escape(self,s):
+    return ''.join('\\u%04X' % ord(c) if ord(c) <= 0xFFFF else '\\u%08X' % ord(c) for c in s)
